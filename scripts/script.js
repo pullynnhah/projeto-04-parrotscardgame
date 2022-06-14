d941eea8c1827204b960238f4f6df0a2f9d7f421
Paula Rodrigues <paulinhaabro@gmail.com>
2022-06-14 00:01:47 -0300

    implement rest of the game logic

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ function createBoard() {
 
 function createCard(frontImage) {
   return `
-  <div class="card" onclick="flip(this)">
+  <div class="card" onclick="userFlip(this, '${frontImage}')">
     <div class="front face">
       <img src="images/${frontImage}" alt="parrot gif"/>
     </div>
@@ -47,10 +47,72 @@ function createCard(frontImage) {
 `;
 }
 
+function flip(card) {
+  card.classList.toggle("flipped");
+}
+
+function userFlip(card, title) {
+  if (canBeFlipped(card)) {
+    flip(card);
+    flipCount++;
+    currentPair.push([card, title]);
+    console.log(currentPair);
+    if (currentPair.length === 2) {
+      validatePair();
+    }
+  }
+}
+
+function validatePair() {
+  if (checkPair()) {
+    currentPair[0][0].classList.add("paired");
+    currentPair[1][0].classList.add("paired");
+    currentPair = [];
+    if (++pairsCount === numberOfCards / 2) {
+      gameOver();
+    }
+  } else {
+    setTimeout(computerFlip, 1000);
+  }
+}
+
+function gameOver() {
+  clearInterval(interval);
+  alert(`Você ganhou em ${flipCount} jogadas!`);
+  const playAgain = prompt("Deseja jogar novamente? [sim/não]");
+  if (playAgain === "sim") {
+    game();
+  }
+}
+
+function computerFlip() {
+  const flippedCards = document.querySelectorAll(".flipped:not(.paired)");
+  for (const flippedCard of flippedCards) {
+    flippedCard.classList.remove("flipped");
+  }
+
+  currentPair = [];
+}
+
+function canBeFlipped(card) {
+  return currentPair.length !== 2 && !card.classList.contains("flipped");
+}
+
+function checkPair() {
+  return currentPair[0][1] === currentPair[1][1];
+}
+
 function game() {
   setNumberOfCards();
   createDeck();
   createBoard();
+
+  currentPair = [];
+  pairsCount = 0;
+  flipCount = 0;
+  time = 0;
+
+  interval = setInterval(() => (timer.textContent = `${++time}`), 1000);
 }
 
 const MIN_CARDS = 4;
@@ -68,5 +130,12 @@ const PARROTS = [
 const main = document.querySelector("main");
 const timer = document.querySelector(".timer");
 
-let deck;
+let time = null;
+let deck = null;
+let interval = null;
+let flipCount = null;
+let pairsCount = null;
+let currentPair = null;
 let numberOfCards = null;
+
+game();
