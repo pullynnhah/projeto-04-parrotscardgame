73bff7f79fb5042a24e8b5e2c702afa8038c4555
Paula Rodrigues <paulinhaabro@gmail.com>
2022-06-13 22:35:22 -0300

    implement prompt

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+function getNumberOfCards() {
+  numberOfCards = Number(
+    prompt(
+      `Com quantas cartas deseja jogar?\nEntre um número par no intervalo: [${MIN_CARDS}, ${MAX_CARDS}]`
+    )
+  );
+
+  if (!isNumberOfCardsValid(numberOfCards)) {
+    getNumberOfCards();
+  }
+}
+
+function isNumberOfCardsValid(n) {
+  return n % 2 === 0 && n >= 4 && n <= 14;
+}
+
+const MIN_CARDS = 4;
+const MAX_CARDS = 14;
 const parrots = [
   "revertitparrot.gif",
   "bobrossparrot.gif",
@@ -7,3 +25,5 @@ const parrots = [
   "explodyparrot.gif",
   "unicornparrot.gif",
 ];
+
+let numberOfCards = null;
